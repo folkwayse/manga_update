03f5d5cde918414b6ad9946de50fdcdfec5a4267
Folkwayse <folkwayse@gmail.com>
2024-06-16 07:55:10 +0700

    add manga update

--- src/kiryu/api.ts
+++ src/kiryu/api.ts
@@ -69,6 +69,76 @@ export const getUpdates = async () => {
   return updateData;
 };
 
+
+export const getMangaUpdate = async ( page: number = 1) => {
+  //   const options = {
+  //     method: "GET",
+  //     url: "http://45.76.148.33:8080/api/kiryuu/v6/summary",
+  //     params: { "": "" },
+  //     headers: {
+  //       "User-Agent": "user-agent: Dart/2.8 (dart:io)",
+  //       "Accept-Encoding": "gzip",
+  //       "Content-Type": "application/json",
+  //     },
+  //   };
+
+  //   const data = axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       return response.data;
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  //   return data;
+
+  const options = {
+    method: "GET",
+    url: "https://kiryuu.id/manga/",
+    params: { page: page, status: "", type: "", order: "update" },
+    headers: { "User-Agent": "insomnia/9.2.0" },
+  };
+
+  const data = await axios
+    .request(options)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  const dom = new JSDOM(data);
+  const document = dom.window.document;
+  // Select elements with class "listupd"
+  const listupdElements = document.querySelectorAll(".listupd");
+  let updateData: any[] = [];
+  // listupdElements.forEach((listupdElement:any) => {
+  //   // Find links within these elements
+  //   listupdElement.querySelectorAll('a[href*="kiryuu.id/manga/"]').forEach((element) => {
+  //     const href = element.href.split("/")
+  //     console.log(href[ href.length - 2 ])
+  //     slugs.push(href[ href.length - 2 ])
+  //   });
+  // });
+  for (const listupdElement of listupdElements) {
+    //add href and chapters number
+    const bsx = listupdElement.querySelectorAll(".bsx");
+    for (const bsxElement of bsx) {
+      const href = bsxElement.querySelector(
+        'a[href*="kiryuu.id/manga/"]'
+      )?.href;
+      const chapters = bsxElement.querySelector(".epxs")?.textContent;
+      const slug = href?.split("/")[href?.split("/").length - 2];
+      const chapter_number = parseFloat(chapters.replace("Chapter", ""));
+      const judul = bsxElement.querySelector("div.tt")?.textContent.trim();
+      const poster = bsxElement.querySelector("img")?.getAttribute("src");
+      updateData.push({ slug, chapter_number, judul, poster });
+    }
+  }
+  // console.log(updateData);
+  return updateData;
+};
 export const getChapters = async (slug: string) => {
   // const options = {
   //   method: "GET",
--- src/router/kiryu.ts
+++ src/router/kiryu.ts
@@ -1,15 +1,18 @@
 import { Hono } from "hono";
-import { getManga, getChapters, getChapterContents } from "../kiryu/api";
+import {
+  getManga,
+  getChapters,
+  getChapterContents,
+  getMangaUpdate,
+} from "../kiryu/api";
 
 const kiryu = new Hono();
 
-
 kiryu.get("/manga/:slug", async (c) => {
-    const slug = c.req.param("slug");
-    const chapters = await getManga(slug);
-    return c.json(chapters);
-  });
-
+  const slug = c.req.param("slug");
+  const chapters = await getManga(slug);
+  return c.json(chapters);
+});
 
 kiryu.get("/chapters/:slug", async (c) => {
   const slug = c.req.param("slug");
@@ -18,8 +21,15 @@ kiryu.get("/chapters/:slug", async (c) => {
 });
 
 kiryu.get("/chaptercontents/:slug", async (c) => {
-    const slug = c.req.param("slug");
-    const chapters = await getChapterContents(slug);
-    return c.json(chapters);
-  });
+  const slug = c.req.param("slug");
+  const chapters = await getChapterContents(slug);
+  return c.json(chapters);
+});
+
+kiryu.get("/mangalist", async (c) => {
+  const page = parseInt(c.req.query("page") || "1", 10);
+  const mangas = await getMangaUpdate(page);
+  return c.json(mangas);
+});
+
 export default kiryu;
